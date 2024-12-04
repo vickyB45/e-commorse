63c4f09940a09d147eb63648aaafb67b986ac122
vimal's portfolio <v.bisht.unic@gmail.com>
2024-12-04 20:13:11 +0530

    delete store

--- store/store.js
+++
@@ -1,8 +0,0 @@
-const ActivateAncorNav = document.querySelectorAll('.ancor');
-
-ActivateAncorNav.forEach(ele => {
-    ele.addEventListener('click', () => {   
-        ActivateAncorNav.forEach(link => link.classList.remove('active'));
-        ele.classList.add('active');
-    });
-});
